9257edbdda831dddb8a9d0404ab0ff3bc486f021
JingYuan <jingyuan0926@gmail.com>
2024-04-27 01:07:32 +0800

    Now all concert can be shown

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,40 @@
 // Home.jsx in Project 2
 import React, { useEffect, useState } from 'react'
 import { EventCard, Slider } from '../components';
-
+import { readContract, resolveMethod } from "thirdweb";
+import { contract } from "../utils/constants";
+import { ethers } from 'ethers';
 const Home = () => {
   const [isLoading, setIsLoading] = useState(true); // Assuming there's an API call
   const [events, setEvents] = useState([]);
 
+
   useEffect(() => {
-    // Simulate API call
-    setTimeout(() => {
-      setEvents(Data); // Replace this with your actual API call
-      setIsLoading(false);
-    }, 1000);
-  }, []); // Empty dependency array means this effect runs once after the first render
+    const fetchEvents = async () => {
+      try {
+        const data = await readContract({
+          contract,
+          method: resolveMethod("getConcerts"),
+          params: []
+        });
+        const formattedData = data.map(event => ({
+          ...event,
+          date: event.date.toString(),
+          ticketPrice: ethers.formatEther(event.ticketPrice.toString()),
+          ticketsSold: event.ticketsSold.toString(),
+          totalSeats: event.totalSeats.toString()
+        }));
+        setEvents(formattedData);
+        console.log(formattedData);
+      } catch (error) {
+        console.error("Failed to fetch events:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchEvents();
+  }, []);
 
   return (
     <div className="bg-gray-100">
