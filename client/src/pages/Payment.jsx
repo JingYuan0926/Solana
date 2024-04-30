d6bbe88205f47589571278159fac1ccd9532fdd1
TzyWei <lingtzywei1114@gmail.com>
2024-04-30 16:56:12 +0800

    pay2_buyTicket

--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -1,10 +1,12 @@
 // Payment.jsx
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { useLocation } from "react-router-dom";
 import { Box, Typography } from "@mui/material";
-import { Pay2 } from '../components'; // Import Pay component
+import { Pay2 } from "../components"; // Import Pay component
+
 
 const Payment = () => {
+
   const location = useLocation();
   const { state: eventData } = location;
 
@@ -12,28 +14,65 @@ const Payment = () => {
 
   console.log(index);
 
-
+  
   return (
     <div className="relative flex-grow bg-gray-100 flex flex-col justify-between">
-
       {/* style bg image*/}
-      <div className="absolute top-0 w-full h-[50vh] bg-cover bg-center " style={{ backgroundImage: `url(${eventData.imageURI})`, backgroundSize: 600, filter: 'blur(13px)' }}></div>
-
+      <div
+        className="absolute top-0 w-full h-[50vh] bg-cover bg-center "
+        style={{
+          backgroundImage: `url(${eventData.imageURI})`,
+          backgroundSize: 600,
+          filter: "blur(13px)",
+        }}
+      ></div>
 
       <div className="pt-[45vh] container mx-auto px-4 flex-grow">
-        <Box style={{ position: 'relative', marginTop: -200, marginBottom: '50px', display: 'flex' }}>
-          <img src={eventData.imageURI} alt={eventData.name} style={{ height: '290px', marginRight: '20px', borderRadius: '10px', marginLeft: '20px' }} />
-          <Typography variant="h6" style={{ flexGrow: 1, fontFamily: 'Georgia, serif', color: 'white', marginLeft: '20px', }}>
+        <Box
+          style={{
+            position: "relative",
+            marginTop: -200,
+            marginBottom: "50px",
+            display: "flex",
+          }}
+        >
+          <img
+            src={eventData.imageURI}
+            alt={eventData.name}
+            style={{
+              height: "290px",
+              marginRight: "20px",
+              borderRadius: "10px",
+              marginLeft: "20px",
+            }}
+          />
+          <Typography
+            variant="h6"
+            style={{
+              flexGrow: 1,
+              fontFamily: "Georgia, serif",
+              color: "white",
+              marginLeft: "20px",
+            }}
+          >
             <br />
-            <span style={{ fontWeight: 'bold', fontSize: '30px' }}>{eventData.name}</span><br />
-            <span style={{ fontWeight: 'bold', fontSize: '30px' }}>in {eventData.venue}</span><br /><br/>
-            <span style={{ fontSize: '20px' }}>#{eventData.genre}</span><br />
+            <span style={{ fontWeight: "bold", fontSize: "30px" }}>
+              {eventData.name}
+            </span>
+            <br />
+            <span style={{ fontWeight: "bold", fontSize: "30px" }}>
+              in {eventData.venue}
+            </span>
+            <br />
+            <br />
+            <span style={{ fontSize: "20px" }}>#{eventData.genre}</span>
+            <br />
           </Typography>
         </Box>
-        <Pay2 eventData={eventData} />
+        <Pay2 eventData={eventData} index={index} />
       </div>
     </div>
   );
 };
 
-export default Payment
+export default Payment;
